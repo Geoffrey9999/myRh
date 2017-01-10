67e5b2d828cd52b69561132299134957f2967b81
geo <geoffrey.leborges@epitech.eu>
2017-01-10 18:08:52 +0100

    users migration done

---
+++ db/migrate/20170110163130_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.date :birthdate
+      t.string :mail
+
+      t.timestamps
+    end
+  end
+end
