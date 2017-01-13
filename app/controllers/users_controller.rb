a99481d66d00e9d6e49557a66fd08e8bac79e3f5
geo <geoffrey.leborges@epitech.eu>
2017-01-13 16:05:35 +0100

    unique mail ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,18 +16,25 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @check_instance = Instance.find_by_id(params[:users].permit(:id_instance)[:id_instance])
-    if @check_instance.present?
+    @check_user = User.find_by(mail: params[:users].permit(:mail)[:mail])
+
+    if @check_user.present?
+      flash[:notice] = 'Le mail est déjà utilisé'
+      redirect_to :controller => 'users', :action => 'index', :error => flash[:notice]
+    elsif @check_instance.nil?
+      flash[:notice] = 'Bien essayé'
+      redirect_to :controller => 'users', :action => 'index', :error => flash[:notice]
+    else
       @users = User.new(params.require(:users).permit(:firstname, :lastname, :birthdate , :mail, :id_instance))
       @users.save
-      flash[:notice] = 'Successfully checked in'
+      flash[:notice] = 'Inscription réussit'
       redirect_to :controller => 'welcome', :action => 'connection'
-    else
-      flash[:notice] = 'Bien Essayé'
-      redirect_to :controller => 'users', :action => 'index', :error => flash[:notice]
     end
-    # render plain: params[:users].inspect
   end
 
 end
+
+
+
+# render plain: params[:users].inspect
